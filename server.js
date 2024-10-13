ce01d3728ab05468de28c87e35897c3f615f89d5
NisHit1Shelar <nishit.shelar23@vit.edu>
2024-10-13 12:19:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,28 +3,50 @@ const path = require("path");
 
 const app = express();
 const server = require("http").createServer(app);
-
 const io = require("socket.io")(server);
 
-app.use(express.static(path.join(__dirname+"/public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
-io.on("connection", function(socket){
-	socket.on("sender-join",function(data){
-		socket.join(data.uid);
-	});
-	socket.on("receiver-join",function(data){
-		socket.join(data.uid);
-		socket.in(data.sender_uid).emit("init", data.uid);
-	});
-	socket.on("file-meta",function(data){
-		socket.in(data.uid).emit("fs-meta", data.metadata);
-	});
-	socket.on("fs-start",function(data){
-		socket.in(data.uid).emit("fs-share", {});
-	});
-	socket.on("file-raw",function(data){
-		socket.in(data.uid).emit("fs-share", data.buffer);
-	})
+io.on("connection", function(socket) {
+    console.log("A user connected");
+
+    // Sender joins the room
+    socket.on("sender-join", function(data) {
+        socket.join(data.uid);
+        console.log(`Sender joined room: ${data.uid}`);
+    });
+
+    // Receiver joins the room
+    socket.on("receiver-join", function(data) {
+        socket.join(data.uid);
+        socket.in(data.sender_uid).emit("init", data.uid);
+        console.log(`Receiver joined room: ${data.uid}`);
+    });
+
+    // Receive and forward file metadata
+    socket.on("file-meta", function(data) {
+        socket.in(data.uid).emit("fs-meta", data.metadata);
+        console.log(`File metadata received for: ${data.metadata.filename}, size: ${data.metadata.total_buffer_size}`);
+    });
+
+    // Receive and forward file chunks (with logging)
+    socket.on("file-raw", function(data) {
+        // Log the chunk size and metadata on the server
+        console.log(`Chunk received: ${data.buffer.length} bytes`);
+        socket.in(data.uid).emit("fs-share", data.buffer);
+    });
+
+    // Signal to the sender to send the next chunk
+    socket.on("fs-next", function() {
+        socket.emit("fs-share", {});
+    });
+
+    socket.on("disconnect", function() {
+        console.log("User disconnected");
+    });
 });
 
-server.listen(5000);
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
